1ffb20f769d4ee50fcb644a8141a7f8a0158f861
SmithLam <lamphuoctuong@gmail.com>
2020-05-27 15:31:46 +0700

    Commit icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import 'react-spinkit'
 
 
 export default class App extends Component {
@@ -8,9 +9,12 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state ={
-      weatherResult:null
+      weatherResult:null,
+      iconURL:""
     }
   }
+
+  // let Spinner
 
   getCurrentWeather = async(lon,lat) =>{
     let API_KEY = process.env.REACT_APP_APIKEY
@@ -18,8 +22,27 @@ export default class App extends Component {
     let data = await fetch(url)
     let result = await data.json()
     console.log("what is the result? ", result)
+    let iconCode = result.weather[0].icon
+    let iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    console.log("What is icon code ", iconCode)
+
     this.setState({weatherResult:result})
+    this.setState({iconURL:iconURL})
   }
+
+  getCityWeather = async(city) =>{
+    let API_KEY = process.env.REACT_APP_APIKEY
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    let data = await fetch(url)
+    let result = await data.json()
+    let iconCode = result.weather[0].icon
+    let iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    console.log("what is the city result? ", result)
+    this.setState({weatherResult:result})
+    this.setState({iconURL:iconURL})
+  }
+
+
 
   getLocation  = () => {
     navigator.geolocation.getCurrentPosition((post) => {
@@ -45,12 +68,15 @@ export default class App extends Component {
     }
 
     return (
-      <div class="body">
+      <div>
         <h1>Weathering You!</h1>
         <h2>{this.state.weatherResult.name}</h2>
-        <h3>{this.state.weatherResult.main.temp}C</h3>
+        <h3>{this.state.weatherResult.main.temp}°C <img src={this.state.iconURL}></img></h3>
         <h3>{this.state.weatherResult.weather[0].description}</h3>
-
+        <button class="btn-primary btn-lg" onClick={()=>this.getCityWeather("Paris")}>Paris, France</button>
+        <button class="btn-primary btn-lg" onClick={()=>this.getCityWeather("Berlin")}>Berlin, Germany</button>
+        <button class="btn-primary btn-lg" onClick={()=>this.getCityWeather("Tokyo")}>Tokyo, Japan</button>
+        <button class="btn-primary btn-lg" onClick={()=>this.getCityWeather("Helsinki")}>Helsinki, Finland</button>
       </div>
     )
   }
