ca926ada1a32f1213cd7b110ce60ade6281af0e1
SmithLam <lamphuoctuong@gmail.com>
2020-05-27 14:31:04 +0700

    Update Weather

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      weatherResult:null
+    }
+  }
+
+  getCurrentWeather = async(lon,lat) =>{
+    let API_KEY = process.env.REACT_APP_APIKEY
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    let data = await fetch(url)
+    let result = await data.json()
+    console.log("what is the result? ", result)
+    this.setState({weatherResult:result})
+  }
+
+  getLocation  = () => {
+    navigator.geolocation.getCurrentPosition((post) => {
+      this.getCurrentWeather(post.coords.longitude, post.coords.latitude)
+    })
+  }
+
+
+  componentDidMount(){
+    console.log("open the app")
+    this.getLocation() //call this only because getCurrentWeather is already in getLocation
+  }
+
+  componentDidUpdate(){
+    console.log("something is updated")
+  }
+
+
+  render() {
+
+    if(this.state.weatherResult == null){
+      return (<div>Loading</div>)
+    }
+
+    return (
+      <div class="body">
+        <h1>Weathering You!</h1>
+        <h2>{this.state.weatherResult.name}</h2>
+        <h3>{this.state.weatherResult.main.temp}C</h3>
+        <h3>{this.state.weatherResult.weather[0].description}</h3>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+
+
