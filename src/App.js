070b7c40e5be8c10b93094ca0adff50cd23330aa
SmithLam <lamphuoctuong@gmail.com>
2020-05-28 08:00:25 +0700

    Commit update UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
 import './App.css';
-import 'react-spinkit'
 import { css } from "@emotion/core";
 import PacmanLoader from "react-spinners/PacmanLoader";
+import umbrellaIcon from '../src/umbrella-icon.png';
 import Moment from 'react-moment';
 import 'moment-timezone';
 
@@ -21,7 +25,6 @@ export default class App extends Component {
     this.state ={
       weatherResult:null,
       iconURL:"",
-      unixTime:""
     }
   }
 
@@ -33,11 +36,9 @@ export default class App extends Component {
     console.log("what is the result? ", result)
     let iconCode = result.list[0].weather[0].icon
     let iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    let unixTime= result.list[0].dt
     console.log("What is icon code ", iconCode)
     this.setState({weatherResult:result})
     this.setState({iconURL:iconURL})
-    this.setState({unixTime:unixTime})
   }
 
   getCityWeather = async(city) =>{
@@ -86,19 +87,78 @@ export default class App extends Component {
     }
 
     return (
-      
-      <div>
-        
-        <h1>Weathering You!</h1>
-        <p className="time"><Moment unix tz="Asia/Ho_Chi_Minh">{this.state.unixTime}</Moment></p>
+        <div>
+     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="sticky-top">
+      <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src={umbrellaIcon}
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '}
+      Weathering You!
+        </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+      <Button className="mr-2" variant="secondary outline-info" onClick={()=>this.getLocation()}>Home</Button>
+      <Button className="mr-2" variant="outline-info" onClick={()=>this.getCityWeather("Paris")}>Paris, France</Button>
+      <Button className="mr-2" variant="outline-info" onClick={()=>this.getCityWeather("Berlin")}>Berlin, Germany</Button>
+      <Button className="mr-2" variant="outline-info" onClick={()=>this.getCityWeather("Tokyo")}>Tokyo, Japan</Button>
+      <Button className="mr-2" variant="outline-info" onClick={()=>this.getCityWeather("Rome")}>Rome, Italy</Button>
+      <Button className="mr-2" variant="outline-info" onClick={()=>this.getCityWeather("London")}>London, UK</Button>
+      <Button className="mr-2" variant="outline-info" onClick={()=>this.getCityWeather("Sydney")}>Sydney, Aus</Button>
+      <Button className="mr-2" variant="outline-info" onClick={()=>this.getCityWeather("Bangkok")}>Bangkok, Thailand</Button>
+      <Button className="mr-2" variant="outline-info" onClick={()=>this.getCityWeather("Shanghai")}>Shanghai, China</Button>
+      </Navbar.Collapse>
+        </Navbar>
+
+        <div className="render">
+       <Row className="display-flex justify-content-center">
+      <Col xs={12} md={4}>
+      <div className="day one">
+        <p className="time">Today: <Moment format="D MMM YYYY" withTitle>{this.state.weatherResult.list[0].dt_txt}</Moment></p>
         <p className="city">{this.state.weatherResult.city.name}</p>
         <p className="temp">{this.state.weatherResult.list[0].main.temp}°C <img alt ={this.state.weatherResult.list[0].weather[0].description} src={this.state.iconURL}></img></p>
         <p className="weather">{this.state.weatherResult.list[0].weather[0].description}</p>
-        <button class="btn-primary btn-lg" onClick={()=>this.getCityWeather("Paris")}>Paris, France</button>
-        <button class="btn-primary btn-lg" onClick={()=>this.getCityWeather("Berlin")}>Berlin, Germany</button>
-        <button class="btn-primary btn-lg" onClick={()=>this.getCityWeather("Tokyo")}>Tokyo, Japan</button>
-        <button class="btn-primary btn-lg" onClick={()=>this.getCityWeather("Helsinki")}>Helsinki, Finland</button>
-      </div>
+        </div>
+      </Col>
+      <Col xs={12} md={4}>
+      <div className="day two">
+        <p className="time"> <Moment format="D MMM YYYY" withTitle>{this.state.weatherResult.list[8].dt_txt}</Moment></p>
+        <p className="city">{this.state.weatherResult.city.name}</p>
+        <p className="temp">{this.state.weatherResult.list[8].main.temp}°C <img alt ={this.state.weatherResult.list[8].weather[0].description} src={this.state.iconURL}></img></p>
+        <p className="weather">{this.state.weatherResult.list[8].weather[0].description}</p>
+        </div>
+      </Col>
+      <Col xs={12} md={4}>
+      <div className="day three">
+        <p className="time"> <Moment format="D MMM YYYY" withTitle>{this.state.weatherResult.list[16].dt_txt}</Moment></p>
+        <p className="city">{this.state.weatherResult.city.name}</p>
+        <p className="temp">{this.state.weatherResult.list[16].main.temp}°C <img alt ={this.state.weatherResult.list[16].weather[0].description} src={this.state.iconURL}></img></p>
+        <p className="weather">{this.state.weatherResult.list[16].weather[0].description}</p>
+        </div>
+      </Col>
+      <Col xs={12} md={4}>
+      <div className="day four">
+        <p className="time"> <Moment format="D MMM YYYY" withTitle>{this.state.weatherResult.list[24].dt_txt}</Moment></p>
+        <p className="city">{this.state.weatherResult.city.name}</p>
+        <p className="temp">{this.state.weatherResult.list[24].main.temp}°C <img alt ={this.state.weatherResult.list[24].weather[0].description} src={this.state.iconURL}></img></p>
+        <p className="weather">{this.state.weatherResult.list[24].weather[0].description}</p>
+        </div>
+      </Col>
+      <Col xs={12} md={4}>
+      <div className="day five">
+        <p className="time"> <Moment format="D MMM YYYY" withTitle>{this.state.weatherResult.list[32].dt_txt}</Moment></p>
+        <p className="city">{this.state.weatherResult.city.name}</p>
+        <p className="temp">{this.state.weatherResult.list[32].main.temp}°C <img alt ={this.state.weatherResult.list[32].weather[0].description} src={this.state.iconURL}></img></p>
+        <p className="weather">{this.state.weatherResult.list[32].weather[0].description}</p>
+        </div> 
+      </Col>
+      </Row>
+
+        </div>
+    </div>
     )
   }
 }
